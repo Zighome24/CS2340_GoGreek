840e2a3bd23f0da87bdd31a36b8554d91176a6e4
zighome24 <justinziegler345@gmail.com>
2017-09-18 21:20:48 -0400

    Base Home Screen Designed and Controller Foundations established

---
+++ app/RatTracker2k17/app/src/main/java/edu/gatech/cs2340/rattracker2k17/Controller/HomeScreenController.java
@@ -0,0 +1,35 @@
+package edu.gatech.cs2340.rattracker2k17.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import edu.gatech.cs2340.rattracker2k17.R;
+
+public class HomeScreenController extends AppCompatActivity {
+
+    private Button btn_Login, btn_NewUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open);
+
+        btn_Login = (Button) findViewById(R.id.btn_login);
+        btn_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btn_NewUser = (Button) findViewById(R.id.btn_newUser);
+        btn_NewUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
