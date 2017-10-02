9bfeb7e7221ef573ec789236df257b4d897908c7
zighome24 <justinziegler345@gmail.com>
2017-10-02 03:34:31 -0400

    Partially finished NewUserController, included current design for handling login events

--- app/RatTracker2k17/app/src/main/java/edu/gatech/cs2340/rattracker2k17/Controller/NewUserScreenController.java
+++ app/RatTracker2k17/app/src/main/java/edu/gatech/cs2340/rattracker2k17/Controller/NewUserScreenController.java
@@ -2,13 +2,20 @@ package edu.gatech.cs2340.rattracker2k17.Controller;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import edu.gatech.cs2340.rattracker2k17.Data.Types;
+import edu.gatech.cs2340.rattracker2k17.Model.User;
 import edu.gatech.cs2340.rattracker2k17.R;
+import edu.gatech.cs2340.rattracker2k17.Service.UserBL;
 
 /**
  * Created by wepperson on 9/24/17.
@@ -19,7 +26,8 @@ public class NewUserScreenController extends AppCompatActivity {
     private static final String LOG_ID = "NewUserScreenController";
 
     private Button btn_createUser, btn_returnHome;
-    private EditText firstName, lastName, email, username, password;
+    private EditText firstName, lastName, email, username, password, cPassword;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +49,34 @@ public class NewUserScreenController extends AppCompatActivity {
         firstName = (EditText) findViewById(R.id.txt_firstName);
         lastName = (EditText) findViewById(R.id.txt_lastName);
         email = (EditText) findViewById(R.id.txt_email);
-        username = (EditText) findViewById(R.id.txt_username);
         password = (EditText) findViewById(R.id.txt_password);
 
-        Log.d(LOG_ID, firstName.getText().toString() + lastName.getText().toString());
 
+        final User nUser = new User(firstName.getText().toString(), lastName.getText().toString(),
+                                email.getText().toString(), Types.SecurityLevel.User, User.NO_UID,
+                                password.getText().toString());
+
+        Log.d(LOG_ID, "createUser: " + nUser.toString());
+
+        final UserBL userBL = new UserBL();
+
+        mAuth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null) {
+                    nUser.setUserID(firebaseAuth.getCurrentUser().getUid());
+                    try {
+                        userBL.addNewUser(nUser);
+                    } catch (Exception e) {
+                        Log.d(LOG_ID, e.getMessage());
+                    }
+                } else {
+                    Toast.makeText(NewUserScreenController.this,
+                            "The user could not be created. "
+                            + "Please check to see if that email already owns an account.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
