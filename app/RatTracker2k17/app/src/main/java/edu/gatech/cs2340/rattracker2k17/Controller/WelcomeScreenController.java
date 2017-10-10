daf154db06abaadb11acdf21919efe4d47078f58
Chris O'Brien <obrienchristopher4@gmail.com>
2017-10-10 12:33:13 -0400

    Added listView and RatSpotting class. Still have to implement database and import data

--- app/RatTracker2k17/app/src/main/java/edu/gatech/cs2340/rattracker2k17/Controller/WelcomeScreenController.java
+++ app/RatTracker2k17/app/src/main/java/edu/gatech/cs2340/rattracker2k17/Controller/WelcomeScreenController.java
@@ -1,15 +1,26 @@
 package edu.gatech.cs2340.rattracker2k17.Controller;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
+
+import edu.gatech.cs2340.rattracker2k17.Model.RatSpotting;
 import edu.gatech.cs2340.rattracker2k17.R;
+
+import static edu.gatech.cs2340.rattracker2k17.R.id.list_view;
 
 /**
  * Created by wepperson on 9/24/17.
@@ -28,6 +39,51 @@ public class WelcomeScreenController extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
         Log.d(LOG_ID, "WelcomeScreenController:onCreate: welcome screen created");
+    }
+
+    public class UsersAdapter extends ArrayAdapter<RatSpotting> {
+        public UsersAdapter(Context context, ArrayList<RatSpotting> spots) {
+            super(context, 0, spots);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            RatSpotting spot = getItem(position);
+
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_user, parent, false);
+            }
+
+            TextView rsDate = (TextView) convertView.findViewById(R.id.rsDate);
+            TextView rsLocationType = (TextView) convertView.findViewById(R.id.rsLocationType);
+            TextView rsBorough = (TextView) convertView.findViewById(R.id.rsBorough);
+
+            rsDate.setText(spot.getDate());
+            rsLocationType.setText(spot.getLocationType());
+            rsBorough.setText(spot.getBorough());
+
+            return convertView;
+        }
+    }
+
+    public void getRatData(View view) {
+
+        ArrayList<RatSpotting> arrayOfUsers = new ArrayList<>();
+//
+//
+//
+//
+//        IMPORT RAT DATA HERE
+//
+//
+//
+//
+//        
+
+        UsersAdapter adapter = new UsersAdapter(this, arrayOfUsers);
+        ListView listView = findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
     }
 
 
--- app/RatTracker2k17/app/src/main/java/edu/gatech/cs2340/rattracker2k17/Model/RatSpotting.java
+++ app/RatTracker2k17/app/src/main/java/edu/gatech/cs2340/rattracker2k17/Model/RatSpotting.java
@@ -1,8 +1,64 @@
 package edu.gatech.cs2340.rattracker2k17.Model;
 
 /**
- * Created by Justin on 10/3/2017.
+ * Created by Chris on 10/9/2017.
  */
 
 public class RatSpotting {
+
+    private String key;
+    public void setKey(String key) { this.key = key;}
+    public String getKey() { return key;}
+
+    private String date;
+    public void setDate(String date) { this.date = date;}
+    public String getDate() { return date;}
+
+    private String locationType;
+    public void setLocationType(String locationType) { this.locationType = locationType;}
+    public String getLocationType() { return locationType;}
+
+    private int zip;
+    public void setZip(int zip) { this.zip = zip;}
+    public int getZip() { return zip;}
+
+    private String address;
+    public void setAddress(String address) { this.address = address;}
+    public String getAddress() { return address;}
+
+    private String city;
+    public void setCity(String city) { this.city = city;}
+    public String getCity() { return city;}
+
+    private String borough;
+    public void setBorough(String borough) { this.borough = borough;}
+    public String getBorough() {return borough;}
+
+    private double lat;
+    public void setLat(double lat) { this.lat = lat;}
+    public double getLat() { return lat;}
+
+    private double lon;
+    public void setLon(double lon) { this.lon = lon;}
+    public double getLon() { return lon;}
+
+    public RatSpotting(String key, String date, String locationType, int zip,
+                       String address, String city, String borough, double lat, double lon) {
+        this.key = key;
+        this.date = date;
+        this.locationType = locationType;
+        this.zip = zip;
+        this.address = address;
+        this.city = city;
+        this.borough = borough;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public RatSpotting() {}
+
+    @Override
+    public String toString() {
+        return date + " " + locationType + " " + borough;
+     }
 }
