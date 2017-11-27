879e648f41bedc3e2d8ca8ca6e9cd2b801067f1f
zighome24 <justinziegler345@gmail.com>
2017-11-27 14:23:55 -0500

    Removing DB persistence on Android device directory

--- app/RatTracker2k17/app/src/main/java/edu/gatech/cs2340/rattracker2k17/Controller/HomeScreenController.java
+++ app/RatTracker2k17/app/src/main/java/edu/gatech/cs2340/rattracker2k17/Controller/HomeScreenController.java
@@ -19,16 +19,16 @@ import edu.gatech.cs2340.rattracker2k17.R;
 public class HomeScreenController extends AppCompatActivity {
 
     private static final String LOG_ID = "HomeScreenController";
-    private Button btn_Login, btn_NewUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         setContentView(R.layout.activity_open);
         RatSpotting.generateNextKey();
         Log.d(LOG_ID, "onCreate:HomeScreenController");
     }
+
+
 
     /**
      * change to log in screen (connect in the "onClick" field in layout)
